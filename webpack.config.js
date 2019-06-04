22dbd81db5f371e5f3d9a59fa0d90079df4c3544
kitajchuk <kitajchuk@gmail.com>
2019-06-04 09:11:30 -0700

    remove docs and watch

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require( "path" );
 const root = path.resolve( __dirname );
 const nodeModules = "node_modules";
 const webpack = require( "webpack" );
-// const BrowserSyncPlugin = require( "browser-sync-webpack-plugin" );
 
 
 
@@ -10,18 +9,7 @@ module.exports = {
     // devtool: "source-map",
 
 
-    plugins: [
-        // new BrowserSyncPlugin({
-        //     open: true,
-        //     host: "localhost",
-        //     port: 3000,
-        //     server: {
-        //         baseDir: ["test"]
-        //         //baseDir: ["docs"],
-        //         //directory: true
-        //     }
-        // })
-    ],
+    plugins: [],
 
 
     resolve: {
